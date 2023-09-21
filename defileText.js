56988c26c9857e54003b49a9b28f391f0e1e4d0d
Mohamed-ux-lab <koureissym@gmail.com>
2023-09-21 09:43:48 +0000

    Mise en page

---
+++ defileText.js
@@ -0,0 +1,13 @@
+export function defileText () {
+    return [
+        " Que vous soyez à la recherche d'une société de nettoyage industriel ou ordinaire, une société d'entretien, du Dépoussiérage, de la Désinfection.",
+
+        "Que vous soyez à la recherche d'un administrateur immobilier pour la gestion de vos maisons, vente et achat de terrain.",
+
+        "Que vous soyez à la recherche d'un logement pour un court séjour,en famille ou un séjour prolongé, nous avons la maison parfaite pour vous.",
+
+        "Que vous soyez à la recherche d'un cabinet de consultation, de conseil et accompagnement."
+    ]
+} 
+
+export default defileText()
