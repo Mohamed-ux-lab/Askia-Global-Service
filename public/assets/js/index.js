3741183107338db8806fff6027d1e718362adfea
Mohamed-ux-lab <koureissym@gmail.com>
2023-10-03 20:50:48 +0000

    Mise à jours du DOM

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,19 +1,3 @@
-import { defileText } from './defileText.js'
 
-const textBox = document.querySelector(".textDefile")
-const nosServices = defileText()
-
-let index = 0
-
-
-function afficherElement() {
-    if (index < nosServices.length){
-        textBox.innerHTML = nosServices[index]
-        index++
-    }else{
-        index = 0
-    }
-    setTimeout(afficherElement, 2000)
-}
-afficherElement()
+// Récuperation des informationd du formulaire
 
