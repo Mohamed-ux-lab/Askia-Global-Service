1432e0bdf1bde1a082f561671ae9c7a611cdaad4
Mohamed-ux-lab <koureissym@gmail.com>
2023-10-04 21:46:58 +0000

    Mise à jours du DOM

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,3 +1,13 @@
+import{ scrollSpy } from '../../lib/ScrollSpy.js'
+
+//On appelle la fonction ScrollSpy de la librairie
+scrollSpy()
 
 // Récuperation des informationd du formulaire
+const myForm = document.getElementById('myForm')
+const myData = new FormData(myForm)
+
+for(const data of myData){
+    // console.log(data[0], data[1])
+}
 
